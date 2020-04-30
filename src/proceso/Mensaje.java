2600c0fcc99ed685e1fdea18059019ab65b1fee0
diegocalle2209 <diegocalle2209@gmail.com>
2020-04-29 22:09:09 -0500

    Se agrego frase

--- src/proceso/Mensaje.java
+++ src/proceso/Mensaje.java
@@ -3,7 +3,8 @@ package proceso;
 public class Mensaje {
 
 	public static void main(String[] args) {
-		System.out.println("En sede Norte");
+		System.out.println("En Sede Norte");
+		System.out.println("La vida es bella");
 
 	}
 
